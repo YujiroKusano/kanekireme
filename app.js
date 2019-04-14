fc70e6871f6152a60528b0533258912eb702f354
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-04-14 16:38:10 +0900

    commit

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.post('/callback', function(req, res) {
                     headers: {
                         'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS + '}'
                     }
-                }
+                };
                 request.get(get_profile_options, function(error, response, body) {
                     if(!error && response.statusCode == 220) {
                         callback(body['displayName']);
