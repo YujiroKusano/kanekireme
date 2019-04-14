ebca459f62b0ba08a707a398890674a27efbcca4
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-04-14 17:47:48 +0900

    commit

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.post('/callback', function(req, res) {
                 };
                 request.get(get_profile_options, function(error, response, body) {
                     if(!error && response.statusCode == 200) {
-                        stage1(body['displayName']);
+                        stage1(body['displayName'], req);
                     }
                 });
             } 
@@ -60,7 +60,7 @@ app.post('/callback', function(req, res) {
     ],
 )});
 
-function stage1(displayName) {
+function stage1(displayName, req) {
     require('dotenv').config();
     var messageText = 'message';
     //ヘッダー部を定義
