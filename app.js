519575cbb7c3f96b56168e1e06b001f0d22420e9
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-04-14 16:13:59 +0900

    commit

--- app.js
+++ app.js
@@ -95,5 +95,5 @@ app.listen(app.get('port'), function() {
 
 function validate_signature(signature, body) {
     return signature == crypto.createHmac('sha256', process.env.LINE_CHANNEL_SECRET).update(
-        new Buffer.alloc(16, JSON.stringify(body), 'utf8')).digest('base64');
+        new Buffer.alloc(1000, JSON.stringify(body), 'utf8')).digest('base64');
 }
