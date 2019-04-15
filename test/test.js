5f8640dc403ba8b4727ee261da6d6088379a3804
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-04-15 23:43:25 +0900

    commt

--- test/test.js
+++ test/test.js
@@ -17,35 +17,31 @@ app.use(bodyParser.json());
 var keyword = 'test';
 app.post('/callback', function(req, res) {
     require('dotenv').config();
-    async.waterfall(
-        function(req) {
-            require('dotenv').config();
-            var messageText = 'message';
-            //ヘッダー部を定義
-            var headers = {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS + '}',
-            };
-            //返信内容を定義        
-            オプションを定義
-            var options = {
-                url: 'https://api.line.me/v2/bot/richmenu',
-                proxy: process.env.FIXIE_URL,
-                headers: headers,
-                json: true,
-                body: btnItem
-            };
+        var messageText = 'message';
+        //ヘッダー部を定義
+        var headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS + '}',
+        };
+        //返信内容を定義        
+        オプションを定義
+        var options = {
+            url: 'https://api.line.me/v2/bot/richmenu',
+            proxy: process.env.FIXIE_URL,
+            headers: headers,
+            json: true,
+            body: btnItem
+        };
 
-            request.post(options, function(error, response, body) {
-                if(!error && response.statusCode == 200) {
-                    console.log(body);
-                    
-                } else {
-                    console.log('error: ' + JSON.stringify(response));
-                }
-            });
-        }
-    )}
+        request.post(options, function(error, response, body) {
+            if(!error && response.statusCode == 200) {
+                console.log(body);
+                
+            } else {
+                console.log('error: ' + JSON.stringify(response));
+            }
+        });
+    }
 );
 
 
