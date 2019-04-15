63dc0b2b5d2f24295128a76214c73af340720550
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-04-15 23:31:14 +0900

    commt

--- test/test.js
+++ test/test.js
@@ -17,60 +17,36 @@ app.use(bodyParser.json());
 var keyword = 'test';
 app.post('/callback', function(req, res) {
     require('dotenv').config();
-    async.waterfall([
-        function(callback) {
-            //個人チャットの場合の処理
-            if(req.body['events'][0]['source']['type'] == 'user') {
-                //ユーザーIDからユーザー名を取得
-                var user_id = req.body['events'][0]['source']['userId'];
-                var get_profile_options = {
-                    url: 'https://api.line.me/v2/bot/profile/' + user_id,
-                    proxy: process.env.FIXIE_URL,
-                    json: true,
-                    headers: {
-                        'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS + '}'
-                    }
-                };
-                request.get(get_profile_options, function(error, response, body) {
-                    if(!error && response.statusCode == 200) {
-                        stage1(body['displayName'], req);   
-                    }
-                });
-            } 
-            //グループチャットの場合の処理
-            else if('room' == req.body['events'][0]['source']['type']) {
-                stage1('あなた', req);
-            }
-        },
-    ],
-    function stage1(req) {
-        require('dotenv').config();
-        var messageText = 'message';
-        //ヘッダー部を定義
-        var headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS + '}',
-        };
-        //返信内容を定義        
-        オプションを定義
-        var options = {
-            url: 'https://api.line.me/v2/bot/richmenu',
-            proxy: process.env.FIXIE_URL,
-            headers: headers,
-            json: true,
-            body: btnItem
-        };
+    async.waterfall(
+        function(req) {
+            require('dotenv').config();
+            var messageText = 'message';
+            //ヘッダー部を定義
+            var headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS + '}',
+            };
+            //返信内容を定義        
+            オプションを定義
+            var options = {
+                url: 'https://api.line.me/v2/bot/richmenu',
+                proxy: process.env.FIXIE_URL,
+                headers: headers,
+                json: true,
+                body: btnItem
+            };
 
-        request.post(options, function(error, response, body) {
-            if(!error && response.statusCode == 200) {
-                console.log(body);
-                
-            } else {
-                console.log('error: ' + JSON.stringify(response));
-            }
-        });
-    }
-)});
+            request.post(options, function(error, response, body) {
+                if(!error && response.statusCode == 200) {
+                    console.log(body);
+                    
+                } else {
+                    console.log('error: ' + JSON.stringify(response));
+                }
+            });
+        }
+    )}
+);
 
 
 app.listen(app.get('port'), function() {
