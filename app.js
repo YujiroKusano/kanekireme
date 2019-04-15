3d524452925fc59a863e642f2364fbe8d9f786f3
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-04-15 23:01:41 +0900

    commt

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var crypto = require('crypto');
 var async = require('async');
-
+var fs = require('fs');
+var button = fs.readFileSync('./config/button.json', 'utf8');
+var btnItem = JSON.parse(button);
 app.set('port', (process.env.PORT || 8000));
 
 // JSONの送信を許可
@@ -108,152 +110,29 @@ function stage1(displayName, req) {
     };
     //返信内容を定義
     var data = {
-        // 'replyToken': req.body['events'][0]['replyToken'],
-        // 'messages': [{
-        //     'type': 'text',
-        //     'text': displayName + messageText,
-        //     "quickReply": {
-        //         "items": [
-        //             {
-        //                 "type": "action",
-        //                 "imageUrl": "https://example.com/sushi.png",
-        //                 "action": {
-        //                     "type": "message",
-        //                     "label": "一覧",
-        //                     "text": "一覧"
-        //                 }
-        //             },
-        //               {
-        //                 "type": "action",
-        //                 "imageUrl": "https://example.com/sushi.png",
-        //                 "action": {
-        //                     "type": "message",
-        //                     "label": "借りる",
-        //                     "text": "借りる"
-        //                 }
-        //             },
-        //             {
-        //                 "type": "action",
-        //                 "imageUrl": "https://example.com/sushi.png",
-        //                 "action": {
-        //                     "type": "message",
-        //                     "label": "貸す",
-        //                     "text": "貸す"
-        //                 }
-        //             },
-        //             {
-        //                 "type": "action",
-        //                 "imageUrl": "https://example.com/sushi.png",
-        //                 "action": {
-        //                     "type": "message",
-        //                     "label": "返済",
-        //                     "text": "返済"
-        //                 }
-        //             },
-        //         ]
-        //     }   
-        // }
-    // ]
-    
-        "size":{
-            "width":2500,
-            "height":1686
-        },
-        "selected":false,
-        "name":"Controller",
-        "chatBarText":"Controller",
-        "areas":[
-            {
-              "bounds":{
-                  "x":551,
-                  "y":325,
-                  "width":321,
-                  "height":321
-              },
-              "action":{
-                  "type":"message",
-                  "text":"up"
-              }
-            },
-            {
-              "bounds":{
-                  "x":876,
-                  "y":651,
-                  "width":321,
-                  "height":321
-              },
-              "action":{
-                  "type":"message",
-                  "text":"right"
-              }
-            },
-            {
-              "bounds":{
-                  "x":551,
-                  "y":972,
-                  "width":321,
-                  "height":321
-              },
-              "action":{
-                  "type":"message",
-                  "text":"down"
-              }
-            },
-            {
-              "bounds":{
-                  "x":225,
-                  "y":651,
-                  "width":321,
-                  "height":321
-              },
-              "action":{
-                  "type":"message",
-                  "text":"left"
-              }
-            },
-            {
-              "bounds":{
-                  "x":1433,
-                  "y":657,
-                  "width":367,
-                  "height":367
-              },
-              "action":{
-                  "type":"message",
-                  "text":"btn b"
-              }
-            },
-            {
-              "bounds":{
-                  "x":1907,
-                  "y":657,
-                  "width":367,
-                  "height":367
-              },
-              "action":{
-                  "type":"message",
-                  "text":"btn a"
-              }
-            }
-        ]
-      
-};
+        'replyToken': req.body['events'][0]['replyToken'],
+        'messages': [{
+            'type': 'text',
+            'text': displayName + messageText,
+            'quickReply': btnItem.quickReply  
+        }
+    ]};
     
     //オプションを定義
-    var options = {
-        url: 'https://api.line.me/v2/bot/richmenu',
-        proxy: process.env.FIXIE_URL,
-        headers: headers,
-        json: true,
-        body: data
-    };
     // var options = {
-    //     url: 'https://api.line.me/v2/bot/message/reply',
+    //     url: 'https://api.line.me/v2/bot/richmenu',
     //     proxy: process.env.FIXIE_URL,
     //     headers: headers,
     //     json: true,
     //     body: data
     // };
+    var options = {
+        url: 'https://api.line.me/v2/bot/message/reply',
+        proxy: process.env.FIXIE_URL,
+        headers: headers,
+        json: true,
+        body: data
+    };
 
     request.post(options, function(error, response, body) {
         if(!error && response.statusCode == 200) {
