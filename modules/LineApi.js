d9ee36bd21a5d2e11f96b3ec54272be237d2b6ef
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-04-21 17:28:07 +0900

    commit

--- modules/LineApi.js
+++ modules/LineApi.js
@@ -56,9 +56,9 @@ exports.postBtn = function(req, button, stage) {
     var resText = 'default';
     if(stage == 0) {
         resText = '相手を選択してください';
-    } else if(stage = 1) {
+    } else if(stage == 1) {
         resText = '金額を入力してください';
-    } else if(stage = 2) {
+    } else if(stage == 2) {
         resText = '詳細を入力してください';
     } else {
 
