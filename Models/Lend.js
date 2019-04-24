68e938c9759a024d93cf7103d1be3eed789134ec
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-04-25 00:59:19 +0900

    test

--- Models/Lend.js
+++ Models/Lend.js
@@ -71,18 +71,13 @@ exports.stage5 = function(user_id, reqText) {
   MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
     // Get the documents collection
     var collection = db.collection('users');
-    var jsDate = new Date();
-    jsDate.setHours(jsDate.getHours() + 9);
     // Update document where status is 1, set partner_id equal to 1
     collection.update(
     { 'user_id': user_id, 'stage': 4},
     { 
       $set: { 
-        stage: 0,
-        detail: reqText,
-        last_date: jsDate.toDateString(),
-        last_time: jsDate.toLocaleTimeString()
-       } 
+        stage: 0
+      }
     })
   })
 };
