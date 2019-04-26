8e155790972795f37cb662ada8e45b63fdf0dfd1
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-04-26 18:15:58 +0900

    test

--- LineApi/Common.js
+++ LineApi/Common.js
@@ -19,7 +19,7 @@ exports.postChecker = function(req, res, callback) {
         || (req.body['events'][0]['message']['type'] != 'text') 
             && (req.body['events'][0]['type'] != 'postback')) {
         console.log('MESSAGE ERROR');
-        console.log(req.body['events']);
+        console.log(req.body['events'][0]);
         return;
     }
     var reqText = req.body['events'][0]['message']['text'];
