873c6931fdb8211d0c46f507c8c49c09770e1fea
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-04-26 19:33:27 +0900

    test

--- Models/Show.js
+++ Models/Show.js
@@ -7,7 +7,7 @@ exports.getUserInfo = function(user_id, callback) {
         // Get the documents collection
         var collection = db.collection('users');
         // Find some documents if user_id and not stage
-        collection.find({ 'user_id': user_id }, function(err, getStatus) {
+        collection.find({ 'user_id': user_id }).toArray(function(err, getStatus) {
             if(getStatus != null) { //成功した場合
                 callback( getStatus );
             } else { //失敗した場合
