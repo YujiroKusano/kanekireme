561e8d0e0a3b0907e7def7bb2df976bfe8ff26f1
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-05-05 16:23:26 +0900

    Added acount

--- LineApi/Common.js
+++ LineApi/Common.js
@@ -11,7 +11,7 @@ var regist = require('./Registration');
 //POSTされた情報を判定する
 exports.postChecker = function(req, res, callback) {
     require('dotenv').config();
-
+    console.log(JSON.stringify(req));
     //LINEから正式に送られてきたかを確認する
     if(!validate_signature(req.headers['x-line-signature'], req.body)) {
         console.log('LINE ERROR');
