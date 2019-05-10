b21767fc30de997cda8afaa08bb06cf88f17f1af
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-05-10 20:27:26 +0900

    test

--- LineApi/Lend.js
+++ LineApi/Lend.js
@@ -2,13 +2,14 @@ var fs = require('fs');
 var button = JSON.parse(fs.readFileSync('./config/lend.json', 'utf8'));
 
 var common = require('./Common');
+var commonDb = require('../Models/Common');
 var lendDb = require('../Models/Lend');
 
 //menu画面を返信する
 exports.postBtn = function(req, user_id, reqText, callback) {
     console.log('aaaaaaaaa');
     require('dotenv').config();
-    commondb.getStage(user_id, function(stage) {
+    commonDb.getStage(user_id, function(stage) {
         var resText = ['相手を選択してください', '金額を入力してください', '詳細を入力してください', 'ボタンを押下してください', '処理が完了しました',];
 
         //返信内容を定義
