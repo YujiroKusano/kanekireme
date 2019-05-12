a99ba6eacebbaea2ad81105fd161dda5a360be7f
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-05-13 07:19:18 +0900

    test

--- LineApi/Lend.js
+++ LineApi/Lend.js
@@ -8,7 +8,7 @@ var lendDb = require('../Models/Lend');
 //menu画面を返信する
 exports.postBtn = function(req, user_id, reqText, callback) {
     require('dotenv').config();
-    console.log(reqText);
+
     commonDb.getStage(user_id, function(stage) {
         
         var resText = ['相手を選択してください', '金額を入力してください', '詳細を入力してください', 'ボタンを押下してください', '処理が完了しました',];
@@ -16,15 +16,14 @@ exports.postBtn = function(req, user_id, reqText, callback) {
         //返信内容を定義
         if(stage == 1) { // 名前登録 -> 金額入力時処理
 
+            // 名前Database登録処理
+            lendDb.runLendStage(user_id, reqText);
+
             // 金額ボタンを送信
             common.postBtn(req, resText[stage], button['stage'][stage], function(result) {
                 console.log('Lend:Button:Result: ' + result);
                 callback(result);
             });
-
-            // 名前Database登録処理
-            lendDb.runLendStage(user_id, reqText);
-
 
         } else if(stage == 2) { // 金額 -> 日付入力時の場合
             
@@ -41,16 +40,18 @@ exports.postBtn = function(req, user_id, reqText, callback) {
                 lendDb.runLendStage(user_id, reqText);
 
             } else {
+                
+                //stage情報を一つ下げる
                 commonDb.cancelStage(user_id);
-                var reqText = '正しい値を入力してください';
-                common.postMsg(req, reqText, function(result) {
+
+                //エラーメッセージを送信
+                var resText = '正しい値を入力してください';
+                common.postMsg(req, resText, function(result) {
                     callback(result);
-                })
+                });
             }
 
-
         } else {
-            
             //Database登録処理
             lendDb.runLendStage(user_id, reqText);
 
@@ -59,9 +60,8 @@ exports.postBtn = function(req, user_id, reqText, callback) {
                 console.log('Lend:Button:Result: ' + result);
                 callback(result);
             });
-
         }
-    })
+    });
 }
 
         
