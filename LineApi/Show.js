c57cbcb08b7aa9ed3b8fdf5990180bee25656c20
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-05-14 23:02:49 +0900

    Added Show DBS

--- LineApi/Show.js
+++ LineApi/Show.js
@@ -8,9 +8,8 @@ exports.postdbs = function(req, user_id, callback) {
     showModels.getPartnerInfo(user_id, function(result){
         var data = new Map();
         console.log('result: ' + JSON.stringify(result));
-        for(var element in JSON.stringify(result)){
-            console.log('element: ' + element);
-            registDb.getAcountName(element['_id'], function(name) {
+        for(var element in result){
+            registDb.getAcountName(result[element]['_id'], function(name) {
                 console.log('name: ' + name );
                 console.log('money' + element['money']);
                 data.set(name, element['money']);
