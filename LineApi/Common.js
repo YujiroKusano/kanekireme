c48819879ae2fe31096fb5b700ff04ac29177fdb
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-05-15 06:50:43 +0900

    Added Show DBS

--- LineApi/Common.js
+++ LineApi/Common.js
@@ -131,6 +131,8 @@ exports.postDPick = function(req, resText, callback) {
 exports.SpecialpostMsg = function(req, data, callback) {
     require('dotenv').config();
 
+    console.loh(data);
+    console.log(JSON.stringify(data));
     //ヘッダー部を定義
     var headers = {
         'Content-Type': 'application/json',
