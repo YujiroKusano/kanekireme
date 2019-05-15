ce47679bb7d29f4b9393d6d644a3ac26a9b93724
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-05-15 22:59:09 +0900

    Added Show DBS

--- LineApi/Show.js
+++ LineApi/Show.js
@@ -7,29 +7,34 @@ var async = require('async');
 exports.postdbs = function(req, user_id, callback) {
     require('dotenv').config();
     var showModels = require('../Models/Show');
+    //返信内容を定義
+    var data = {
+        'replyToken': req.body['events'][0]['replyToken'],
+    };
+    var rpdata= [];
     async.waterfall([
         function (callback) {
             //返信内容を定義    
             showModels.getPartnerInfo(user_id, function(result){
-                var resText;
-                for(var element in result){
-                    registDb.getAcountName(result[element]['_id'], function(name) {
-                        console.log('name: ' + name );
-                        console.log('money: ' + result[element]['money']);
-                        resText += name + ': ' +result[element]['money'] + '\n';                   
-                    })
-                }
-                callback(null, resText);
-            });
-        },
-        function(err, resText){
-            console.log(resText)
-            common.postMsg(req, resText, function(result){
-                callback(result)
+                //var resText;
+                // for(var element in result){
+                //     registDb.getAcountName(result[element]['_id'], function(name) {
+                //         console.log('name: ' + name );
+                //         console.log('money: ' + result[element]['money']);
+                //         rpdata = result[element];                   
+                //     })
+                // }
+                data = result;
+                callback(null, data);
             });
         }
-    ]
-    )
+    ],
+    function(err, resText){
+        console.log(resText)
+        common.postMsg(req, resText, function(result){
+            callback(result)
+        });
+    })
 }
 
 // exports.postdbs = function(req, user_id, callback) {
