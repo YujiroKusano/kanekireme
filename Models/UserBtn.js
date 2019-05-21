7d94dd6eb24046e82ab87c46ea092fa782a9106b
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-05-21 21:30:01 +0900

    Added Show Database

--- Models/UserBtn.js
+++ Models/UserBtn.js
@@ -77,7 +77,7 @@ exports.getUserName = function(user_id, callback) {
   MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
       assert.equal(null, err);
       // Get the documents collection
-      var collection = db.collection('users');
+      var collection = db.collection(userButton);
       // Find some documents if user_id and not stage
       collection.find({ 'user_id': user_id }).toArray(function(err, getStatus) {
           if(getStatus != null) { //成功した場合
