27d1072fadfa6d38547a6ecd96e204dffd013bc2
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-05-22 07:15:44 +0900

    Added Show Database

--- LineApi/Show.js
+++ LineApi/Show.js
@@ -6,6 +6,7 @@ exports.postdbs = function(req, user_id, callback) {
     require('dotenv').config();
     //返信内容を定義    
     showModels.getLentInfo(user_id, function(result){
+        console.log('itirann: ' + result)
         var resText = '';
         for (test in result) {
             if(test > 0) {
@@ -13,10 +14,14 @@ exports.postdbs = function(req, user_id, callback) {
             }
             resText += result[test]['_id'] + ' / ' + result[test]['money'];
         }
-        common.postMsg(req, resText, function(result) {
-            console.log('itirann: ' + resText)
-            callback(null, result);
-        })
+        if(resText != null) {
+            common.postMsg(req, resText, function(result) {
+                callback(null, result);
+            })
+        } else {
+            console.log('itiran::error')
+        }
+
     });
 
 }
