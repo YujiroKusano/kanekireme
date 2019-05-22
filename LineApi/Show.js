6d9c8b121415d3da671bd5a116d33d480ce7d8a7
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-05-22 21:50:56 +0900

    test

--- LineApi/Show.js
+++ LineApi/Show.js
@@ -10,26 +10,59 @@ exports.postdbs = function(req, user_id, callback) {
             console.log('Lend: ' + JSON.stringify(LendResult));
             console.log('Rent: ' + JSON.stringify(RentResult));
             var resText = '';
-            for (test in LendResult) {
-                if(test > 0) {
-                    resText += '\n';
-                }
-                if(RentResult[test]) {
-                    if(RentResult[test]['_id'] == LendResult[test]['_id']) {
-                        resText += LendResult[test]['_id'] + ' / ' + (LendResult[test]['money'] + (RentResult[test]['money'] * -1));
-                    }
-                } else {
-                    resText += LendResult[test]['_id'] + ' / ' + LendResult[test]['money']
-                }
-            }
-            for (test in RentResult) {
-                if(!LendResult[test]) {
-                    if(resText) {
+            var findFlag = false;
+            // for (test in LendResult) {
+            //     if(test > 0) {
+            //         resText += '\n';
+            //     }
+            //     if(RentResult[test]) {
+            //         if(RentResult[test]['_id'] == LendResult[test]['_id']) {
+            //             resText += LendResult[test]['_id'] + ' / ' + (LendResult[test]['money'] + (RentResult[test]['money'] * -1));
+            //         }
+            //     } else {
+            //         resText += LendResult[test]['_id'] + ' / ' + LendResult[test]['money']
+            //     }
+            // }
+            // for (test in RentResult) {
+            //     if(!LendResult[test]) {
+            //         if(resText) {
+            //             resText += '\n';
+            //         }
+            //         resText += REndResult[test]['_id'] + ' / ' + (RentResult[test]['money'] * -1);
+            //     } 
+            // }
+            /**貸借計算処理 */
+            for (LendCnt in LendResult) {
+                for (RentCnt in RentResult) {
+                    if(LendCnt > 0 || LendCnt > 0) {
                         resText += '\n';
                     }
-                    resText += REndResult[test]['_id'] + ' / ' + (RentResult[test]['money'] * -1);
-                } 
+                    if(RentResult[RentCnt]['_id'] == LendResult[LendCnt]['_id']) {  //両方に存在した場合
+                        resText += LendResult[LendCnt]['_id'] + ' / ' + (LendResult[LendCnt]['money'] + (RentResult[RentCnt]['money'] * -1));
+                        findFlag = true;
+                        break;
+                    }
+                }
+                if(!findFlag) { //Lendのみ存在していた場合
+                    resText += LendResult[LendCnt]['_id'] + ' / ' + LendResult[LendCnt]['money'];
+                } else { //両方に存在した場合
+                    findFlag = false;
+                }
             }
+            for (RentCnt in RentResult) {
+                for (LendCnt in LendResult) {
+                    if(RentResult[RentCnt]['_id'] == LendResult[LendCnt]['_id']) {
+                        findFlag = true;
+                        break;
+                    }
+                }
+                if(!findFlag) { //Rentのみ存在していた場合
+                    resText += RentResult[RentCnt]['_id'] + ' / ' + (RentResult[RentCnt]['money'] * -1);
+                } else { //両方に存在した場合
+                    findFlag = false;
+                }
+            }
+
             if(resText != null) {
                 common.postMsg(req, resText, function(result) {
                     callback(result);
