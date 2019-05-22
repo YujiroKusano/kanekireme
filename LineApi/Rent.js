90258037b36aba6faddf38fb41b3c6d502c97665
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-05-22 20:29:17 +0900

    test

---
+++ LineApi/Rent.js
@@ -0,0 +1,130 @@
+let fs = require('fs');
+let button = JSON.parse(fs.readFileSync('./config/button.json', 'utf8'));
+
+let common = require('./Common');
+let commonDb = require('../Models/Common');
+let RentDb = require('../Models/Rent');
+let registDb = require('../Models/Registration');
+let userBtnDb = require('../Models/UserBtn');
+
+//menu画面を返信する
+exports.postBtn = function(req, user_id, reqText, callback) {
+    require('dotenv').config();
+
+    commonDb.getStage(user_id, function(stage) {
+        
+        var resText = ['相手を選択してください', '金額を入力してください', '詳細を入力してください', 'ボタンを押下してください', '処理が完了しました',];
+
+        //返信内容を定義
+        if(stage == 1) { // 名前登録 -> 金額入力時処理
+            registDb.alreadyName(reqText,function(result) {
+                if(result) {
+                    registDb.getAcountName(user_id, function(name) { //LINEAPIから名前を取得 
+                        // 相手のIDをデータベースに登録
+                        registDb.getAcountId(reqText, function(result) {   
+                            // 名前Database登録処理
+                            RentDb.runRentStage(user_id, reqText, result, name);
+                        });
+                    });
+
+                    // 金額ボタンを送信
+                    common.postBtn(req, resText[stage], button['stage'][stage], function(result) {
+                        console.log('Rent:Button:Result: ' + result);
+                        callback(result);
+                    });
+                } else {
+                    // 金額ボタンを送信
+                    userBtnDb.getUserButton(user_id, function(button) {
+
+                        //からではないことを確認
+                        if(button != 0) {
+                            //相手を選択してくださいボタンを表示
+                            var eResText = '正しいユーザーを選択してください';
+                            common.postBtn(req, eResText, button, function(result){
+                                callback(result);
+                            });
+                        } else {
+                            console.error('情報不正：ユーザ取得の際にDBが操作されました')
+                        }
+                    })
+                    //エラーメッセージを送信
+                }
+
+            })
+            
+        } else if(stage == 2) { // 金額 -> 詳細入力ボタンを表示
+
+            // 金額が正常に入力されているかを判定
+            if(!isNaN(Number(reqText))){
+                console.log('reqtext: 金額: ' + reqText);
+                // 金額をDatabaseに登録
+                RentDb.runRentStage(user_id, Number(reqText));
+
+                // 日付ピッカーを送信
+                common.postBtn(req, resText[stage],  button['stage'][stage], function(result){
+                    console.log('Rent:DayPicker:Result: ' + result);
+                    callback(result);
+                });
+
+            } else {
+                                
+                //エラーメッセージを送信
+                var resText = '正しい金額を入力してください';
+                common.postMsg(req, resText, function(result) {
+                    callback(result);
+                });
+            }
+
+        } else if(stage == 3) { // 詳細 -> 日付入力ボタンを表示
+            var str_obj = new String(reqText);
+            var len = str_obj.length;
+
+            if(len <= 30) {
+                // 詳細をDatabaseに登録
+                RentDb.runRentStage(user_id, reqText);
+
+                // 日付ピッカーを送信
+                common.postDPick(req, resText[stage], function(result){
+                    console.log('Rent:DayPicker:Result: ' + result);
+                    callback(result);
+                });
+            } else {
+                
+                //エラーメッセージを送信
+                var resText = '30文字以内で入力してください';
+                common.postMsg(req, resText, function(result) {
+                    callback(result);
+                });
+            }
+            
+
+        } else if(stage == 4){ //日付 -> 完了ボタンを表示
+ 
+            //requestの中身が日付ッピッカーから入力された情報かを判定
+            if(req.body['events'][0]['type'] === 'postback') {
+
+                //日付をDatabaseに登録
+                RentDb.runRentStage(user_id, reqText);
+
+                // 完了ボタンを送信
+                common.postBtn(req, resText[stage], button['stage'][stage], function(result) {
+                    console.log('Rent:Button:Result: ' + result);
+                    callback(result);
+                });
+
+            } else { //日付以外のデータが送信された場合
+
+                //エラーメッセージを送信
+                var resText = '正しい日付を入力してください';
+                common.postMsg(req, resText, function(result) {
+                    callback(result);
+                });
+
+            }
+
+        }
+    });
+}
+
+        
+
---
+++ Models/Rent.js
@@ -0,0 +1,109 @@
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+var commondb = require('./Common');
+exports.runRentStage = function(user_id, reqText, pertner_id, name)  {
+  commondb.getStage(user_id, (stage) => {
+    if(stage == 0) {
+      console.log('Rent::stage情報不正: ' + reqText);
+    } else if(stage == 1) {
+      console.log('Rent::stage１:: stage2実行: ' + reqText);
+      stage2(user_id, pertner_id, reqText, name);
+    } else if(stage == 2) {
+      console.log('Rent::stage2:: stage3実行: ' + reqText);
+      stage3(user_id, reqText);
+    } else if(stage == 3) {
+      console.log('Rent::stage3:: stage4実行: ' + reqText);
+      stage4(user_id, reqText);
+    } else if(stage == 4) {
+      console.log('Rent::stage4:: stage5実行: ' + reqText);
+      stage5(user_id, reqText);
+    } else if(stage == 5) {
+      console.log('Rent::stage5:: stage6実行: ' + reqText);
+      stage6(user_id, reqText);
+    }
+  });
+}
+  //stage2
+var stage2 = function(user_id, partner_id, reqText, name) {
+    MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+      // Get the documents collection
+      console.log(user_id + ': stage1');
+      var collection = db.collection('users');
+      var jsDate = new Date();
+      jsDate.setHours(jsDate.getHours() + 9);
+      // Update document where status is 1, set partner_id equal to 1
+      collection.update(
+      { 'Sign_id': user_id, 'stage': 1},
+      { 
+        $inc: { stage: 1 },
+        $set: { 
+          Lend_id: partner_id,
+          Lend_name: reqText,
+          Rent_id: user_id,
+          Rent_name: name,
+          timeStamp: jsDate
+         } 
+      });
+    });
+};
+
+ //stage2
+var stage3 = function(user_id, reqText) {
+  MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+    // Get the documents collection
+    var collection = db.collection('users');
+    var jsDate = new Date();
+    jsDate.setHours(jsDate.getHours() + 9);
+    // Update document where status is 1, set partner_id equal to 1
+    collection.update(
+    { 'Sign_id': user_id, 'stage': 2},
+    { 
+      $inc: { stage: 1 },
+      $set: { 
+        money: reqText,
+        timeStamp: jsDate
+       } 
+    });
+  });
+};
+
+//stage3
+var stage4 = function(user_id, reqText) {
+  MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+    // Get the documents collection
+    var collection = db.collection('users');
+    var jsDate = new Date();
+    jsDate.setHours(jsDate.getHours() + 9);
+    // Update document where status is 1, set partner_id equal to 1
+    collection.update(
+    { 'Sign_id': user_id, 'stage': 3},
+    { 
+      $inc: { stage: 1 },
+      $set: { 
+        detail: reqText,
+        timeStamp: jsDate
+       } 
+    });
+  });
+};
+
+//stage4
+var stage5 = function(user_id, reqDate) {
+  MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+    // Get the documents collection
+    var collection = db.collection('users');
+    var jsDate = new Date();
+    jsDate.setHours(jsDate.getHours() + 9);
+    // Update document where status is 1, set partner_id equal to 1
+    collection.update(
+    { 'Sign_id': user_id, 'stage': 4},
+    { 
+      $set: { 
+        stage: 0, 
+        date: reqDate,
+        timeStamp: jsDate
+       } 
+    });
+  });
+};
