ec747ac9c40afbfc99ddab2500f5a22dab059cca
Kusano Yujiro <yujiro.kusano.1106@gmail.com>
2019-05-22 20:01:26 +0900

    Added Show Database

--- LineApi/Show.js
+++ LineApi/Show.js
@@ -5,26 +5,29 @@ var showModels = require('../Models/Show');
 exports.postdbs = function(req, user_id, callback) {
     require('dotenv').config();
     //返信内容を定義    
-    showModels.getLentInfo(user_id, function(result){
-        console.log('itirann: ' + JSON.stringify(result));
-        var resText = '';
-        for (test in result) {
-            if(test > 0) {
-                resText += '\n';
-            }
-            resText += result[test]['_id'] + ' / ' + result[test]['money'];
-        }
-        if(resText != null) {
-            common.postMsg(req, resText, function(result) {
-                callback(result);
-            })
-        } else {
-            let eResText = 'err'
-            common.postMsg(req, eResText, function(result) {
-                callback(result);
-            })
-        }
-
+    showModels.getLentInfo(user_id, function(LendResult){
+        showModels.getLentInfo(user_id, function(RentResult) {
+            console.log('Lend: ' + JSON.stringify(LendResult));
+            console.log('Rent: ' + JSON.stringify(RentResult));
+            callback(true);
+        })
+        // var resText = '';
+        // for (test in result) {
+        //     if(test > 0) {
+        //         resText += '\n';
+        //     }
+        //     resText += result[test]['_id'] + ' / ' + result[test]['money'];
+        // }
+        // if(resText != null) {
+        //     common.postMsg(req, resText, function(result) {
+        //         callback(result);
+        //     })
+        // } else {
+        //     let eResText = 'err'
+        //     common.postMsg(req, eResText, function(result) {
+        //         callback(result);
+        //     })
+        // }
     });
 
 }
--- Models/Show.js
+++ Models/Show.js
@@ -16,3 +16,39 @@ exports.getUserInfo = function(user_id, callback) {
         });
     });
 }
+
+exports.getLendInfo = function(user_id, callback) {
+    MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+        // Get the documents collection
+        var collection = db.collection('users');
+        collection.aggregate([
+            { $match: { Lend_id: user_id } },
+            { $group: {  _id: Rent_name, money: { $sum: '$money' } } }
+        ]).toArray(function(err, status) {
+            if((status != null) || (status != undefined)) { //成功した場合
+                callback( status );
+            } else { //失敗した場合
+                console.error(err);
+                callback( 0 );
+            }
+        });
+    });
+}
+
+exports.getRentInfo = function(user_id, callback) {
+    MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+        // Get the documents collection
+        var collection = db.collection('users');
+        collection.aggregate([
+            { $match: { Rent_id: user_id } },
+            { $group: {  _id: Lend_name, money: { $sum: '$money' } } }
+        ]).toArray(function(err, status) {
+            if((status != null) || (status != undefined)) { //成功した場合
+                callback( status );
+            } else { //失敗した場合
+                console.error(err);
+                callback( 0 );
+            }
+        });
+    });
+}
